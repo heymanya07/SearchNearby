1d2ab0e88711dcdadcff2678a1cf09e085638d74
MANYA SINHA <111224399+heymanya07@users.noreply.github.com>
2022-09-23 23:48:02 +0530

    Update DirectionStepAdapter.kt

--- app/src/main/java/com/example/nearmekotlindemo/adapter/DirectionStepAdapter.kt
+++ app/src/main/java/com/example/nearmekotlindemo/adapter/DirectionStepAdapter.kt
@@ -1,82 +1,48 @@
-package com.example.nearmekotlindemo.activities
+package com.example.nearmekotlindemo.adapter
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.biometric.BiometricPrompt
-import android.util.Log
-import android.widget.Toast
-import androidx.core.content.ContextCompat
-import com.example.nearmekotlindemo.R
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import java.util.concurrent.Executor
+import androidx.recyclerview.widget.RecyclerView
 
-class SplashActivity : AppCompatActivity() {
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.os.Build
+import android.text.Html
+import com.example.nearmekotlindemo.models.googlePlaceModel.directionPlaceModel.DirectionStepModel
+import com.example.nearmekotlindemo.databinding.StepItemLayoutBinding
 
-    private lateinit var executor: Executor
-    private lateinit var biometricPrompt: BiometricPrompt
-    private lateinit var promptInfo: androidx.biometric.BiometricPrompt.PromptInfo
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        executor = ContextCompat.getMainExecutor(this)
-        biometricPrompt = androidx.biometric.BiometricPrompt(this, executor,
-            object : BiometricPrompt.AuthenticationCallback() {
-                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                    super.onAuthenticationError(errorCode, errString)
-                    Toast.makeText(
-                        this@SplashActivity,
-                        "Authentication error $errString",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                }
-
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                }
-
-                override fun onAuthenticationFailed() {
-                    super.onAuthenticationFailed()
-
-                    Toast.makeText(
-                        this@SplashActivity,
-                        "Authentication failed ",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                }
-            })
-
-        promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Biometric Authentication")
-            .setSubtitle("Finger print authentication required to login")
-            .setNegativeButtonText("Use account password")
-            .build()
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (Firebase.auth.currentUser != null) {
-
-                val finger = getSharedPreferences("user", MODE_PRIVATE)
-                    .getBoolean("finger", false)
-                if (finger) {
-                    biometricPrompt.authenticate(promptInfo)
-                } else {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-                }
-
-            } else {
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
-            }
-        }, 3000)
+class DirectionStepAdapter : RecyclerView.Adapter<DirectionStepAdapter.ViewHolder>() {
+    private var directionStepModels: List<DirectionStepModel>? = null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = StepItemLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+        )
+        return ViewHolder(binding)
     }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (directionStepModels != null) {
+            val (distance, duration, _, htmlInstructions) = directionStepModels!![position]
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                holder.binding.txtStepHtml.text =
+                    Html.fromHtml(htmlInstructions, Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                holder.binding.txtStepHtml.text = Html.fromHtml(htmlInstructions)
+            }
+            holder.binding.txtStepTime.text = duration!!.text
+            holder.binding.txtStepDistance.text = distance!!.text
+        }
+    }
 
+    override fun getItemCount(): Int {
+        return if (directionStepModels != null) directionStepModels!!.size else 0
+    }
+
+    fun setDirectionStepModels(directionStepModels: List<DirectionStepModel>?) {
+        this.directionStepModels = directionStepModels
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(val binding: StepItemLayoutBinding) : RecyclerView.ViewHolder(
+        binding.root
+    )
 }
